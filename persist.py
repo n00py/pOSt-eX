618e5bd71f2f56c7305932f0995a7f715140f675
n00py <esteban.m.rodriguez@gmail.com>
2016-09-13 21:29:13 -0700

    Update persist.py

--- persist.py
+++ persist.py
@@ -66,7 +66,7 @@ class Module:
             'Trigger' : {
                 'Description'   :   'The trigger word.',
                 'Required'      :   True,
-                'Value'         :   'Harambe'
+                'Value'         :   ''
             }
         }
 #
